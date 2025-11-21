4a4a87aa631d8b259e18a13afd7f9131b5e25950
Al Ain <82544587+darkjive@users.noreply.github.com>
2025-11-21 14:04:19 +0100

    init composer

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
+    'A\\WohlfuehlgesundheitDe\\' => array($baseDir . '/src'),
 );
