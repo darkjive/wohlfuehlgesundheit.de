2c3a7d619bfb13614bc84f3be9a781beed508b26
Al Ain <82544587+darkjive@users.noreply.github.com>
2025-11-27 08:04:01 +0100

    update content

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -41,6 +41,6 @@ export const footerData = {
     },
   ],
   footNote: `
-    © Wohlfühlgesundheit - Holistische Darmtherapie<br /> <a class="text-primary hover:text-black dark:text-secondary" href="/impressum">Impressum</a> · <a class="text-primary hover:text-black dark:text-secondary" href="/datenschutz">Datenschutz</a> · <a class="text-primary hover:text-black dark:text-secondary" href="/agb">AGB</a> · <a class="text-primary hover:text-black dark:text-secondary" href="/widerrufsbelehrung">Widerrufsbelehrung</a>
+    <p class="mb-1">© Wohlfühlgesundheit - Holistische Darmtherapie</p><br /> <a class="text-primary hover:text-black dark:text-secondary" href="/impressum">Impressum</a> · <a class="text-primary hover:text-black dark:text-secondary" href="/datenschutz">Datenschutz</a> · <a class="text-primary hover:text-black dark:text-secondary" href="/agb">AGB</a> · <a class="text-primary hover:text-black dark:text-secondary" href="/widerrufsbelehrung">Widerrufsbelehrung</a>
   `,
 };
